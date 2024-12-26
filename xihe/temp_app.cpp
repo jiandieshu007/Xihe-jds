8f63d4c45fa5278f46ccf80cae4abdb34b776ab7
jiandieshu007 <1697107197@qq.com>
2024-12-26 11:48:23 +0800

    pull

--- xihe/temp_app.cpp
+++ xihe/temp_app.cpp
@@ -45,41 +45,41 @@ bool TempApp::prepare(Window *window)
 	auto *p_cascade_script = cascade_script.get();
 	scene_->add_component(std::move(cascade_script));
 
-	// shadow pass
-	{
-		PassAttachment shadow_attachment_0{AttachmentType::kDepth, "shadowmap"};
-		shadow_attachment_0.extent_desc                         = ExtentDescriptor::Fixed({kShadowmapResolution, kShadowmapResolution, 1});
-		shadow_attachment_0.image_properties.array_layers  = 3;
-		shadow_attachment_0.image_properties.current_layer = 0;
+	//// shadow pass
+	//{
+	//	PassAttachment shadow_attachment_0{AttachmentType::kDepth, "shadowmap"};
+	//	shadow_attachment_0.extent_desc                         = ExtentDescriptor::Fixed({kShadowmapResolution, kShadowmapResolution, 1});
+	//	shadow_attachment_0.image_properties.array_layers  = 3;
+	//	shadow_attachment_0.image_properties.current_layer = 0;
 
-		PassAttachment shadow_attachment_1      = shadow_attachment_0;
-		shadow_attachment_1.image_properties.current_layer = 1;
+	//	PassAttachment shadow_attachment_1      = shadow_attachment_0;
+	//	shadow_attachment_1.image_properties.current_layer = 1;
 
-		PassAttachment shadow_attachment_2      = shadow_attachment_0;
-		shadow_attachment_2.image_properties.current_layer = 2;
+	//	PassAttachment shadow_attachment_2      = shadow_attachment_0;
+	//	shadow_attachment_2.image_properties.current_layer = 2;
 
-		auto shadow_pass_0 = std::make_unique<CascadeShadowPass>(scene_->get_components<sg::Mesh>(), *p_cascade_script, 0);
-		graph_builder_->add_pass("Shadow 0", std::move(shadow_pass_0))
-		    .attachments({{shadow_attachment_0}})
-		    .shader({"shadow/csm.vert", "shadow/csm.frag"})
-		    .finalize();
+	//	auto shadow_pass_0 = std::make_unique<CascadeShadowPass>(scene_->get_components<sg::Mesh>(), *p_cascade_script, 0);
+	//	graph_builder_->add_pass("Shadow 0", std::move(shadow_pass_0))
+	//	    .attachments({{shadow_attachment_0}})
+	//	    .shader({"shadow/csm.vert", "shadow/csm.frag"})
+	//	    .finalize();
 
-		auto shadow_pass_1 = std::make_unique<CascadeShadowPass>(scene_->get_components<sg::Mesh>(), *p_cascade_script, 1);
-		graph_builder_->add_pass("Shadow 1", std::move(shadow_pass_1))
-		    .attachments({{shadow_attachment_1}})
-		    .shader({"shadow/csm.vert", "shadow/csm.frag"})
-		    .finalize();
+	//	auto shadow_pass_1 = std::make_unique<CascadeShadowPass>(scene_->get_components<sg::Mesh>(), *p_cascade_script, 1);
+	//	graph_builder_->add_pass("Shadow 1", std::move(shadow_pass_1))
+	//	    .attachments({{shadow_attachment_1}})
+	//	    .shader({"shadow/csm.vert", "shadow/csm.frag"})
+	//	    .finalize();
 
-		auto shadow_pass_2 = std::make_unique<CascadeShadowPass>(scene_->get_components<sg::Mesh>(), *p_cascade_script, 2);
-		graph_builder_->add_pass("Shadow 2", std::move(shadow_pass_2))
-		    .attachments({{shadow_attachment_2}})
-		    .shader({"shadow/csm.vert", "shadow/csm.frag"})
-		    .finalize();
-	}
+	//	auto shadow_pass_2 = std::make_unique<CascadeShadowPass>(scene_->get_components<sg::Mesh>(), *p_cascade_script, 2);
+	//	graph_builder_->add_pass("Shadow 2", std::move(shadow_pass_2))
+	//	    .attachments({{shadow_attachment_2}})
+	//	    .shader({"shadow/csm.vert", "shadow/csm.frag"})
+	//	    .finalize();
+	//}
 
 	// geometry pass
 	{
-		/*auto geometry_pass = std::make_unique<GeometryPass>(scene_->get_components<sg::Mesh>(), *camera);
+		auto geometry_pass = std::make_unique<GeometryPass>(scene_->get_components<sg::Mesh>(), *camera);
 
 		graph_builder_->add_pass("Geometry", std::move(geometry_pass))
 
@@ -89,106 +89,106 @@ bool TempApp::prepare(Window *window)
 
 		    .shader({"deferred/geometry.vert", "deferred/geometry.frag"})
 
-		    .finalize();*/
-
-		auto geometry_pass = std::make_unique<MeshletPass>(scene_->get_components<sg::Mesh>(), *camera);
-
-		graph_builder_->add_pass("Geometry", std::move(geometry_pass))
-
-		    .attachments({{AttachmentType::kDepth, "depth"},
-		                  {AttachmentType::kColor, "albedo"},
-		                  {AttachmentType::kColor, "normal", vk::Format::eA2B10G10R10UnormPack32}})
-
-		    .shader({"deferred/geometry_mesh.task", "deferred/geometry_mesh.mesh", "deferred/geometry_mesh.frag"})
-
 		    .finalize();
+
+		//auto geometry_pass = std::make_unique<MeshletPass>(scene_->get_components<sg::Mesh>(), *camera);
+
+		//graph_builder_->add_pass("Geometry", std::move(geometry_pass))
+
+		//    .attachments({{AttachmentType::kDepth, "depth"},
+		//                  {AttachmentType::kColor, "albedo"},
+		//                  {AttachmentType::kColor, "normal", vk::Format::eA2B10G10R10UnormPack32}})
+
+		//    .shader({"deferred/geometry_mesh.task", "deferred/geometry_mesh.mesh", "deferred/geometry_mesh.frag"})
+
+		//    .finalize();
 	}
 
-	// lighting pass
-	{
-		auto lighting_pass = std::make_unique<LightingPass>(scene_->get_components<sg::Light>(), *camera, p_cascade_script);
+	//// lighting pass
+	//{
+	//	auto lighting_pass = std::make_unique<LightingPass>(scene_->get_components<sg::Light>(), *camera, p_cascade_script);
 
-		graph_builder_->add_pass("Lighting", std::move(lighting_pass))
+	//	graph_builder_->add_pass("Lighting", std::move(lighting_pass))
 
-		    .bindables({{BindableType::kSampled, "depth"},
-		                {BindableType::kSampled, "albedo"},
-		                {BindableType::kSampled, "normal"},
-		                {BindableType::kSampled, "shadowmap"}})
+	//	    .bindables({{BindableType::kSampled, "depth"},
+	//	                {BindableType::kSampled, "albedo"},
+	//	                {BindableType::kSampled, "normal"},
+	//	                {BindableType::kSampled, "shadowmap"}})
 
-		    .attachments({{AttachmentType::kColor, "lighting", vk::Format::eR16G16B16A16Sfloat}})
+	//	    .attachments({{AttachmentType::kColor, "lighting", vk::Format::eR16G16B16A16Sfloat}})
 
-		    .shader({"deferred/lighting.vert", "deferred/lighting.frag"})
+	//	    .shader({"deferred/lighting.vert", "deferred/lighting.frag"})
 
-		    .finalize();
-	}
+	//	    .finalize();
+	//}
 
-	// bloom pass
-	{
-		auto extract_pass = std::make_unique<BloomExtractPass>();
+	//// bloom pass
+	//{
+	//	auto extract_pass = std::make_unique<BloomExtractPass>();
 
-		graph_builder_->add_pass("Bloom Extract", std::move(extract_pass))
-		    .bindables({{BindableType::kSampled, "lighting"},
-		                {BindableType::kStorageWrite, "bloom_extract", vk::Format::eR16G16B16A16Sfloat, ExtentDescriptor::SwapchainRelative(0.5,0.5)}})
-		    .shader({"post_processing/bloom_extract.comp"})
-		    .finalize();
+	//	graph_builder_->add_pass("Bloom Extract", std::move(extract_pass))
+	//	    .bindables({{BindableType::kSampled, "lighting"},
+	//	                {BindableType::kStorageWrite, "bloom_extract", vk::Format::eR16G16B16A16Sfloat, ExtentDescriptor::SwapchainRelative(0.5,0.5)}})
+	//	    .shader({"post_processing/bloom_extract.comp"})
+	//	    .finalize();
 
-		auto downsample_pass0 = std::make_unique<BloomDownsamplePass>(1.0f);
-		graph_builder_->add_pass("Bloom Downsample 0", std::move(downsample_pass0))
-		    .bindables({{BindableType::kSampled, "bloom_extract"},
-		                {BindableType::kStorageWrite, "bloom_down_sample_0", vk::Format::eR16G16B16A16Sfloat, ExtentDescriptor::SwapchainRelative(0.25, 0.25)}})
-		    .shader({"post_processing/bloom_downsample.comp"})
-		    .finalize();
+	//	auto downsample_pass0 = std::make_unique<BloomDownsamplePass>(1.0f);
+	//	graph_builder_->add_pass("Bloom Downsample 0", std::move(downsample_pass0))
+	//	    .bindables({{BindableType::kSampled, "bloom_extract"},
+	//	                {BindableType::kStorageWrite, "bloom_down_sample_0", vk::Format::eR16G16B16A16Sfloat, ExtentDescriptor::SwapchainRelative(0.25, 0.25)}})
+	//	    .shader({"post_processing/bloom_downsample.comp"})
+	//	    .finalize();
 
-		auto downsample_pass1 = std::make_unique<BloomDownsamplePass>(0.9f);
-		graph_builder_->add_pass("Bloom Downsample 1", std::move(downsample_pass1))
-		    .bindables({{BindableType::kSampled, "bloom_down_sample_0"},
-		                {BindableType::kStorageWrite, "bloom_down_sample_1", vk::Format::eR16G16B16A16Sfloat, ExtentDescriptor::SwapchainRelative(0.125, 0.125)}})
-		    .shader({"post_processing/bloom_downsample.comp"})
-		    .finalize();
+	//	auto downsample_pass1 = std::make_unique<BloomDownsamplePass>(0.9f);
+	//	graph_builder_->add_pass("Bloom Downsample 1", std::move(downsample_pass1))
+	//	    .bindables({{BindableType::kSampled, "bloom_down_sample_0"},
+	//	                {BindableType::kStorageWrite, "bloom_down_sample_1", vk::Format::eR16G16B16A16Sfloat, ExtentDescriptor::SwapchainRelative(0.125, 0.125)}})
+	//	    .shader({"post_processing/bloom_downsample.comp"})
+	//	    .finalize();
 
-		auto downsample_pass2 = std::make_unique<BloomDownsamplePass>(0.7f);
-		graph_builder_->add_pass("Bloom Downsample 2", std::move(downsample_pass2))
-		    .bindables({{BindableType::kSampled, "bloom_down_sample_1"},
-		                {BindableType::kStorageWrite, "bloom_down_sample_2", vk::Format::eR16G16B16A16Sfloat, ExtentDescriptor::SwapchainRelative(0.0625, 0.0625)}})
-		    .shader({"post_processing/bloom_downsample.comp"})
-		    .finalize();
+	//	auto downsample_pass2 = std::make_unique<BloomDownsamplePass>(0.7f);
+	//	graph_builder_->add_pass("Bloom Downsample 2", std::move(downsample_pass2))
+	//	    .bindables({{BindableType::kSampled, "bloom_down_sample_1"},
+	//	                {BindableType::kStorageWrite, "bloom_down_sample_2", vk::Format::eR16G16B16A16Sfloat, ExtentDescriptor::SwapchainRelative(0.0625, 0.0625)}})
+	//	    .shader({"post_processing/bloom_downsample.comp"})
+	//	    .finalize();
 
-		auto upsample_pass0 = std::make_unique<BloomComputePass>();
-		graph_builder_->add_pass("Bloom Upsample 0", std::move(upsample_pass0))
-		    .bindables({{BindableType::kSampled, "bloom_down_sample_2"},
-		                {BindableType::kStorageWrite, "bloom_up_sample_0", vk::Format::eR16G16B16A16Sfloat, ExtentDescriptor::SwapchainRelative(0.125, 0.125)}})
-		    .shader({"post_processing/bloom_upsample.comp"})
-		    .finalize();
+	//	auto upsample_pass0 = std::make_unique<BloomComputePass>();
+	//	graph_builder_->add_pass("Bloom Upsample 0", std::move(upsample_pass0))
+	//	    .bindables({{BindableType::kSampled, "bloom_down_sample_2"},
+	//	                {BindableType::kStorageWrite, "bloom_up_sample_0", vk::Format::eR16G16B16A16Sfloat, ExtentDescriptor::SwapchainRelative(0.125, 0.125)}})
+	//	    .shader({"post_processing/bloom_upsample.comp"})
+	//	    .finalize();
 
-		auto upsample_pass1 = std::make_unique<BloomComputePass>();
-		graph_builder_->add_pass("Bloom Upsample 1", std::move(upsample_pass1))
-		    .bindables({{BindableType::kSampled, "bloom_up_sample_0"},
-		                {BindableType::kStorageWrite, "bloom_up_sample_1", vk::Format::eR16G16B16A16Sfloat, ExtentDescriptor::SwapchainRelative(0.25, 0.25)}})
-		    .shader({"post_processing/bloom_upsample.comp"})
-		    .finalize();
+	//	auto upsample_pass1 = std::make_unique<BloomComputePass>();
+	//	graph_builder_->add_pass("Bloom Upsample 1", std::move(upsample_pass1))
+	//	    .bindables({{BindableType::kSampled, "bloom_up_sample_0"},
+	//	                {BindableType::kStorageWrite, "bloom_up_sample_1", vk::Format::eR16G16B16A16Sfloat, ExtentDescriptor::SwapchainRelative(0.25, 0.25)}})
+	//	    .shader({"post_processing/bloom_upsample.comp"})
+	//	    .finalize();
 
-		auto upsample_pass2 = std::make_unique<BloomComputePass>();
-		graph_builder_->add_pass("Bloom Upsample 2", std::move(upsample_pass2))
-		    .bindables({{BindableType::kSampled, "bloom_up_sample_1"},
-		                {BindableType::kStorageWrite, "bloom_up_sample_2", vk::Format::eR16G16B16A16Sfloat, ExtentDescriptor::SwapchainRelative(0.5, 0.5)}})
-		    .shader({"post_processing/bloom_upsample.comp"})
-		    .finalize();
-	}
+	//	auto upsample_pass2 = std::make_unique<BloomComputePass>();
+	//	graph_builder_->add_pass("Bloom Upsample 2", std::move(upsample_pass2))
+	//	    .bindables({{BindableType::kSampled, "bloom_up_sample_1"},
+	//	                {BindableType::kStorageWrite, "bloom_up_sample_2", vk::Format::eR16G16B16A16Sfloat, ExtentDescriptor::SwapchainRelative(0.5, 0.5)}})
+	//	    .shader({"post_processing/bloom_upsample.comp"})
+	//	    .finalize();
+	//}
 
-	gui_ = std::make_unique<Gui>(*this, *window, stats_.get());
-	// composite pass
-	{
-		auto composite_pass = std::make_unique<BloomCompositePass>();
-		graph_builder_->add_pass("Bloom Composite", std::move(composite_pass))
-		    .bindables({{BindableType::kSampled, "lighting"},
-		                {BindableType::kSampled, "bloom_up_sample_2"}})
-		    .shader({"post_processing/bloom_composite.vert", "post_processing/bloom_composite.frag"})
-		    .gui(gui_.get())
-		    .present()
-		    .finalize();
-	}
-	{
-	}
+	//gui_ = std::make_unique<Gui>(*this, *window, stats_.get());
+	//// composite pass
+	//{
+	//	auto composite_pass = std::make_unique<BloomCompositePass>();
+	//	graph_builder_->add_pass("Bloom Composite", std::move(composite_pass))
+	//	    .bindables({{BindableType::kSampled, "lighting"},
+	//	                {BindableType::kSampled, "bloom_up_sample_2"}})
+	//	    .shader({"post_processing/bloom_composite.vert", "post_processing/bloom_composite.frag"})
+	//	    .gui(gui_.get())
+	//	    .present()
+	//	    .finalize();
+	//}
+	//{
+	//}
 
 	graph_builder_->build();
 
